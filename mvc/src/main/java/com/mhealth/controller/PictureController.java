c91314bfcf377596a7b7de6593aa3e1563e530d1
drholy <pyk6512008@sina.com>
2016-05-27 15:55:50 +0800

    下载文件源转到swift

--- mvc/src/main/java/com/mhealth/controller/PictureController.java
+++ mvc/src/main/java/com/mhealth/controller/PictureController.java
@@ -1,10 +1,12 @@
 package com.mhealth.controller;
 
 import com.mhealth.common.util.StringUtils;
+import com.mhealth.openstack.jclouds.JCloudsSwift;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 
@@ -15,14 +17,18 @@ import java.io.*;
 @RequestMapping("pic")
 public class PictureController {
 
+    @Resource(name = "jCloudsSwift")
+    private JCloudsSwift jCloudsSwift;
+
     @RequestMapping(value = "getUserImg/{fileName}", produces = {"image/jpeg;charset=UTF-8"})
     public void getUserImg(@PathVariable String fileName, HttpServletResponse response) {
         InputStream in = null;
         OutputStream out = null;
         try {
             if (StringUtils.isEmpty(fileName)) response.getWriter().println("文件名不能为空");
-            File file = new File("D:\\images\\userImgs", fileName);
-            in = new FileInputStream(file);
+//            File file = new File("D:\\images\\userImgs", fileName);
+//            in = new FileInputStream(file);
+            in = jCloudsSwift.getObject(fileName);
             out = response.getOutputStream();
             int temp;
             while ((temp = in.read()) != -1) {
